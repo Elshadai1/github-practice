7b619cbbb77e10abda90811647b364bff9363135
Elshaday Teshome <elshadayteshome@Elshadays-MBP.localdomain>
2019-02-15 13:45:38 -0800

    updated oddOrEven.js

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,12 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
-}
-
-oddOrEven(4)
-oddOrEven(11)
-oddOrEven(75)
+function oddOrEven(number){
+  if(number % 2 === 0){
+    return "Even"
+  } else {
+    return "ODD"
+  };
+};
+  console.log('The result is ' + oddOrEven(4));
+  console.log('The result is ' + oddOrEven(11));
+  console.log('The result is ' + oddOrEven(75));
